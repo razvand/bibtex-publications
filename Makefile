5d6d868278e353b638e4817ab79a6fa806fa0cb5
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2020-12-27 18:37:16 +0200

    Add BibTeX / LaTeX files to manage publications

---
+++ Makefile
@@ -0,0 +1,24 @@
+BASENAME = publications
+PDF = $(addsuffix .pdf, $(BASENAME))
+TEX = $(addsuffix .tex, $(BASENAME))
+BIB = $(addsuffix .bib, $(BASENAME))
+PDFLATEX = pdflatex
+BIBTEX = bibtex
+OUT_DIR = texfiles
+
+.PHONY: clean all
+
+all: $(PDF)
+
+$(PDF): $(TEX) $(BIB)
+	# Create out directory.
+	-test -d $(OUT_DIR) || mkdir $(OUT_DIR)
+	# Run twice, so TOC is also updated.
+	$(PDFLATEX) -output-directory $(OUT_DIR) -jobname $(basename $@) $<
+	$(BIBTEX) $(OUT_DIR)/$(basename $@)
+	$(PDFLATEX) -output-directory $(OUT_DIR) -jobname $(basename $@) $<
+	ln -f $(OUT_DIR)/$@ .
+
+clean:
+	-test -d $(OUT_DIR) && rm -fr $(OUT_DIR)
+	-rm -f $(PDF) $(PDF_EN)
---
+++ bib_printer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+
+import bibtexparser
+
+with open('publications.bib') as f:
+    bibdb = bibtexparser.load(f)
+
+for e in bibdb.entries:
+    y = e['year']
+    t = e['title']
+    doi = ""
+    if 'doi' in e.keys():
+        doi = e['doi']
+    wos = ""
+    if 'note' in e.keys():
+        if e['note'].find('WOS') != -1:
+            wos = e['note'].split(':')[1]
+    if e['ENTRYTYPE'] == 'inproceedings':
+        j = e['booktitle']
+    elif e['ENTRYTYPE'] == 'article':
+        v = ''
+        n = ''
+        if 'volume' in e.keys():
+            v = e['volume']
+        if 'number' in e.keys():
+            n = e['number']
+        j = "{}, vol. {}, num. {}".format(e['journal'], v, n)
+    issn = ""
+    isbn = ""
+    if 'isbn' in e:
+        isbn = e['isbn']
+    if 'issn' in e:
+        issn = e['issn']
+    print u"{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(y, t, doi, wos, "", j, issn, "", isbn)
